001a16ef8bfb281a743fc04c221fd876d588c2d7
chris <5sensprod@gmail.com>
2023-06-27 09:03:35 +0200

    style Score component

--- src/components/Score/index.jsx
+++ src/components/Score/index.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
-import { PieChart, Pie, Cell, Tooltip } from 'recharts'
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts'
+import styles from './Score.module.css'
 
 const Score = ({ score }) => {
   const data = [
     { name: 'Score', value: score * 100 },
-    { name: 'Rest', value: (1 - score) * 100 }, // Ajout d'une portion pour représenter le reste du cercle
+    { name: 'Rest', value: (1 - score) * 100 },
   ]
 
   return (
-    <div>
-      <h2>Score</h2>
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data}
-          cx={200}
-          cy={200}
-          innerRadius={60}
-          outerRadius={80}
-          paddingAngle={5}
-          dataKey="value"
-        >
-          <Cell fill="#8884d8" />{' '}
-          {/* Couleur de la portion représentant le score */}
-          <Cell fill="#ccc" />{' '}
-          {/* Couleur de la portion représentant le reste */}
-        </Pie>
-        <Tooltip />
-      </PieChart>
+    <div className={styles.scoreContainer}>
+      <h2 className={styles.scoreTitle}>Score</h2>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            innerRadius={70}
+            outerRadius={85}
+            paddingAngle={5}
+            dataKey="value"
+            startAngle={90}
+          >
+            <Cell fill="#ff0000" />
+            <Cell fill="#FBFBFB" />
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
+      <p className={styles.scoreText}>
+        <span className={styles.scoreNumber}>{`${score * 100}%`}</span>
+        <br />
+        de votre
+        <br />
+        objectif
+      </p>
     </div>
   )
 }
