c3e92ce364cdf203feee5ea3e36a4c60df6a1a24
chris <5sensprod@gmail.com>
2023-06-28 04:01:35 +0200

    Gestion de l'erreur l'ID not found

--- src/services/serviceApi.js
+++ src/services/serviceApi.js
@@ -11,20 +11,33 @@ import ActivityModel from '../models/ActivityModel'
 
 export const fetchUser = (id) => {
   const data = userData[id]
+  if (!data) {
+    return Promise.reject(new Error(`No user with id ${id}`))
+  }
   return Promise.resolve(new UserModel(data))
 }
-
 export const fetchPerformance = (userId) => {
   const data = performanceData[userId]
+  if (!data) {
+    return Promise.reject(new Error(`No performance data for user ${userId}`))
+  }
   return Promise.resolve(new PerformanceModel(data))
 }
 
 export const fetchAverageSessions = (userId) => {
   const data = averageSessionsData[userId]
+  if (!data) {
+    return Promise.reject(
+      new Error(`No average session data for user ${userId}`)
+    )
+  }
   return Promise.resolve(new AverageSessionsModel(data))
 }
 
 export const fetchActivity = (userId) => {
   const data = activityData[userId]
+  if (!data) {
+    return Promise.reject(new Error(`No activity data for user ${userId}`))
+  }
   return Promise.resolve(new ActivityModel(data))
 }
