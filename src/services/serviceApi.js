15a397eb4e623f9180b1e9335657bbf1313bc252
chris <5sensprod@gmail.com>
2023-06-28 18:32:29 +0200

    Refacto serviceApi.js

--- src/services/serviceApi.js
+++ src/services/serviceApi.js
@@ -8,6 +8,27 @@ import PerformanceModel from '../models/PerformanceModel'
 import AverageSessionsModel from '../models/AverageSessionsModel'
 import ActivityModel from '../models/ActivityModel'
 
+const API_BASE_URL = 'http://localhost:5000/user/'
+
+const fetchData = async (userId, resource, Model, mockData) => {
+  if (process.env.REACT_APP_USE_MOCK_DATA === 'true') {
+    const data = mockData[userId]
+    if (!data) {
+      throw new Error(`No ${resource} data for user ${userId}`)
+    }
+    return new Model(data)
+  } else {
+    try {
+      const response = await fetch(`${API_BASE_URL}${userId}/${resource}`)
+      const data = await response.json()
+      return new Model(data.data)
+    } catch (error) {
+      console.error(`Failed to fetch ${resource} data`, error)
+      throw error
+    }
+  }
+}
+
 export const fetchUser = async (id) => {
   if (process.env.REACT_APP_USE_MOCK_DATA === 'true') {
     try {
@@ -23,7 +44,7 @@ export const fetchUser = async (id) => {
     }
   } else {
     try {
-      const response = await fetch(`http://localhost:5000/user/${id}`)
+      const response = await fetch(`${API_BASE_URL}${id}`)
       const data = await response.json()
       return new UserModel(data.data)
     } catch (error) {
@@ -34,64 +55,18 @@ export const fetchUser = async (id) => {
 }
 
 export const fetchPerformance = async (userId) => {
-  if (process.env.REACT_APP_USE_MOCK_DATA === 'true') {
-    const data = performanceData[userId]
-    if (!data) {
-      throw new Error(`No performance data for user ${userId}`)
-    }
-    return new PerformanceModel(data)
-  } else {
-    try {
-      const response = await fetch(
-        `http://localhost:5000/user/${userId}/performance`
-      )
-      const data = await response.json()
-      return new PerformanceModel(data.data)
-    } catch (error) {
-      console.error('Failed to fetch performance data', error)
-      throw error
-    }
-  }
+  return fetchData(userId, 'performance', PerformanceModel, performanceData)
 }
 
 export const fetchAverageSessions = async (userId) => {
-  if (process.env.REACT_APP_USE_MOCK_DATA === 'true') {
-    const data = averageSessionsData[userId]
-    if (!data) {
-      throw new Error(`No average session data for user ${userId}`)
-    }
-    return new AverageSessionsModel(data)
-  } else {
-    try {
-      const response = await fetch(
-        `http://localhost:5000/user/${userId}/average-sessions`
-      )
-      const data = await response.json()
-      return new AverageSessionsModel(data.data)
-    } catch (error) {
-      console.error('Failed to fetch average sessions data', error)
-      throw error
-    }
-  }
+  return fetchData(
+    userId,
+    'average-sessions',
+    AverageSessionsModel,
+    averageSessionsData
+  )
 }
 
 export const fetchActivity = async (userId) => {
-  if (process.env.REACT_APP_USE_MOCK_DATA === 'true') {
-    const data = activityData[userId]
-    if (!data) {
-      throw new Error(`No activity data for user ${userId}`)
-    }
-    return new ActivityModel(data)
-  } else {
-    try {
-      const response = await fetch(
-        `http://localhost:5000/user/${userId}/activity`
-      )
-      const data = await response.json()
-      return new ActivityModel(data.data)
-    } catch (error) {
-      console.error('Failed to fetch activity data', error)
-      throw error
-    }
-  }
+  return fetchData(userId, 'activity', ActivityModel, activityData)
 }
