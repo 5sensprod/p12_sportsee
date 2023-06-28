23a5e52ca538d8c0654ffb6d13751b6dc5dd95fb
chris <5sensprod@gmail.com>
2023-06-28 04:03:55 +0200

    Stylisation page notFound

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import styles from './NotFound.module.css'
 
-function NotFound() {
+// Le composant NotFound affiche une page d'erreur 404 personnalisable.
+
+function NotFound({
+  message = "Oups ! La page que vous demandez n'existe pas.",
+  returnLink = '/',
+}) {
   return (
-    <div>
-      <h1>404 - Page non trouvée</h1>
-    </div>
+    <main className={styles.error}>
+      <p className={styles.errorCode}>404</p>
+      <p className={styles.errorMessage}>{message}</p>
+      <p className={styles.errorBackLink}>
+        <Link to={returnLink}>Retourner sur la page d'accueil</Link>
+      </p>
+    </main>
   )
+}
+
+// Les props du composant NotFound ont des valeurs par défaut, donc .isRequired n'est pas nécessaire
+
+NotFound.propTypes = {
+  message: PropTypes.string,
+  returnLink: PropTypes.string,
 }
 
 export default NotFound
